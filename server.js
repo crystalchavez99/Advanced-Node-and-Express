0bbb7429fc78774c088d6b511d8c9aad83c3106b
Crystal <crystal120599@gmail.com>
2023-01-01 22:30:17 -0800

    Set up a Template Engine

--- server.js
+++ server.js
@@ -10,9 +10,13 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+// Assigns setting name to value.
+app.set('view engine', 'pug');
+app.set('views', './views/pug')
 app.route('/').get((req, res) => {
-
+  // Renders a view and sends the rendered HTML string to the client. Optional parameters:
+  // send the rendered view to the client
+  res.render('index')
 });
 
 const PORT = process.env.PORT || 3000;
