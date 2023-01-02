08585ae65ce8aff918a00acf5f329d30d050d46e
Crystal <crystal120599@gmail.com>
2023-01-02 12:37:57 -0800

    setup passport!

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+// authentication middleware for Node.js
+const passport = require('passport');
+const  session  = require('express-session');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
@@ -10,6 +13,18 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// This is used to compute the hash used to encrypt your cookie!
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Assigns setting name to value.
 app.set('view engine', 'pug');
 app.set('views', './views/pug')
