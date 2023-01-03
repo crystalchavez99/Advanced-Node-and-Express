46e7d0acb6f3021b1b211d918d5a8b25e1bdc66f
Crystal <crystal120599@gmail.com>
2023-01-03 11:12:22 -0800

    log user out

--- server.js
+++ server.js
@@ -70,6 +70,17 @@ myDB(async client => {
     res.render('profile', {user: req.user.username})
   })
 
+  app.route('/logout').get((res,res)=>{
+    req.logout();
+    res.redirect('/');
+  })
+
+  app.use((req,res,next)=>{
+    res.status(404)
+    .type('text')
+    .send('NOT FOUND')
+  })
+
 // persist user data (after successful authentication) into session.
   passport.serializeUser((user, done) => {
     done(null, user._id);
